eef70a0fe33c04517020068baf47501fb07e0476
Hayuros <hayuros.peixer@gmail.com>
2021-04-13 22:42:56 -0300

    Falta adicionar a message box. O resto está tudo ok.

--- ExercicioCliente/MenuCadastraCliente.cs
+++ ExercicioCliente/MenuCadastraCliente.cs
@@ -5,91 +5,154 @@ using System.Drawing;
 namespace TelaCLiente
 {
     public class MenuCadastraCLiente : Form{
+
+        Button btnCadastra;
+        Button btnCancela;
+
+        Label lbTextoInicial;
+        Label lbNome;
+        Label lbAniversario;
+        Label lbIdentificacao;
+        Label lbDiasRetorno;
+
+        TextBox tbNome;
+        TextBox tbAniversario;
+        TextBox tbIdentificacao;
+
+        ComboBox cbDiasRetorno;
+
+        GroupBox gbSexo;
+
+        RadioButton rbHomem;
+        RadioButton rbMulher;
+        RadioButton rbNaoInformado;
+
+        // MessageBox mbClienteCadastrado;
+
         public MenuCadastraCLiente() {
             //Criando os Botões
-            Button Cadastra = new Button() {
+            btnCadastra = new Button() {
                 Text = "Cadastrar!",
-                Location = new Point(15, 180),
+                Location = new Point(15, 275),
                 BackColor = System.Drawing.Color.Green
             };
-            Button Cancela = new Button() {
+            btnCadastra.Click += new EventHandler(this.btnCadastraClick);
+            
+            btnCancela = new Button() {
                 Text = "Cancelar!",
-                Location = new Point(100, 180),
+                Location = new Point(100, 275),
                 BackColor = System.Drawing.Color.Red
             };
+            btnCancela.Click += new EventHandler(this.btnCancelaClick);
 
             //Criando as Labels
-            Label textoInicial = new Label() {
+            lbTextoInicial = new Label() {
                 Text = "Cadastrando o Cliente",
-                Location = new Point(80, 1),
+                Location = new Point(40, 1),
                 Size = new Size(155, 12)
                 
             };
-            Label nomeLabel = new Label() {
+            lbNome = new Label() {
                 Text = "Nome",
                 Location = new Point(5, 35)
             };
-            Label aniversarioLabel = new Label() {
+            lbAniversario = new Label() {
                 Text = "Aniversario",
                 Location = new Point(5, 70)
             };
-            Label identificacaoLabel = new Label() {
+            lbIdentificacao = new Label() {
                 Text = "Identificacao",
                 Location = new Point(5, 105)
             };
-            Label diasRetornoLabel = new Label() {
+            lbDiasRetorno = new Label() {
                 Text = "DiasRetorno",
                 Location = new Point(5, 140)
             };
             
-            //Criando as Text Box's
-            TextBox nomeTextBox = new TextBox() {
-                Text = "Nome",
+            //Criando as Text Box
+            tbNome = new TextBox() {
                 Location = new Point(105, 35),
                 Size = new Size(100, 10)
             };
-            TextBox aniversarioTextBox = new TextBox() {
-                Text = "Aniversario",
+            tbAniversario = new TextBox() {
                 Location = new Point(105, 70),
                 Size = new Size(100, 10)
             };
-            TextBox identificacaoTextBox = new TextBox() {
-                Text = "Identificacao",
+            tbIdentificacao = new TextBox() {
                 Location = new Point(105, 105),
                 Size = new Size(100, 10)
             };
-            TextBox diasRetornoTextBox = new TextBox() {
-                Text = "DiasRetorno",
+
+            //Criando as Combo Box
+            cbDiasRetorno = new ComboBox() {
                 Location = new Point(105, 140),
                 Size = new Size(100, 10)
+            };      
+            cbDiasRetorno.Items.AddRange(new string[] {"5", "10", "15", "20"});
+
+            //Criando as Group Box
+            gbSexo = new GroupBox() {
+                Text = "Sexo",
+                Location =  new Point(5, 175),
+                Size = new Size(200, 80)
             };
 
+            //Criando os Radio Button
+            rbHomem = new RadioButton() {
+                Text = "Homem",
+                Location = new Point(8, 15)
+            };
+            rbMulher = new RadioButton() {
+                Text = "Mulher",
+                Location = new Point(8, 35)
+            };
+            rbNaoInformado = new RadioButton() {
+                Text = "NaoInformado",
+                Location = new Point(8, 55)
+            };
+
+            //Criando as Message Box
+            // mbClienteCadastrado = new MessageBox();
 
             //Adcionando os Objetos a tela
             this.Text = "Cadastro de Clientes";
 
             //Adicionando os Botões
-            this.Controls.Add(Cadastra);
-            this.Controls.Add(Cancela);
+            this.Controls.Add(btnCadastra );
+            this.Controls.Add(btnCancela);
 
             //Adicionando as Labels
-            this.Controls.Add(textoInicial);
-
-            this.Controls.Add(nomeLabel);
-            this.Controls.Add(aniversarioLabel);
-            this.Controls.Add(identificacaoLabel);
-            this.Controls.Add(diasRetornoLabel);
+            this.Controls.Add(lbTextoInicial);
+            this.Controls.Add(lbNome);
+            this.Controls.Add(lbAniversario);
+            this.Controls.Add(lbIdentificacao);
+            this.Controls.Add(lbDiasRetorno);
 
             // Adicionando as Text Box
-            this.Controls.Add(nomeTextBox);
-            this.Controls.Add(aniversarioTextBox);
-            this.Controls.Add(identificacaoTextBox);
-            this.Controls.Add(diasRetornoTextBox);
+            this.Controls.Add(tbNome);
+            this.Controls.Add(tbAniversario);
+            this.Controls.Add(tbIdentificacao);
+
+            // Adicionando as Combo Box
+            this.Controls.Add(cbDiasRetorno);
             
+            //Adicionando as Group Box
+            this.Controls.Add(gbSexo);
             
+            //Adicionando os Radio Button
+            gbSexo.Controls.Add(rbHomem);
+            gbSexo.Controls.Add(rbMulher);
+            gbSexo.Controls.Add(rbNaoInformado);
+                        
             //Setando o Tamanho
-            this.Size = new Size(300, 250);
+            this.Size = new Size(220, 350);
             Application.Run(this);
+        }
+        private void btnCancelaClick(object sender, EventArgs e) {
+            this.Close();
+        }
+        private void btnCadastraClick(object sender, EventArgs e) {
+            this.Close();
         }
     }
 }
--- ExercicioCliente/Program.cs
+++ ExercicioCliente/Program.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Drawing;
+using TelaCLiente;
 
 namespace ExercicioCliente
 {
-    public class Program
+    public class Program : Form
     {
-        public static void Main() 
-        { 
-            Form menuPrincipal = new Form() {
-                Size = new Size(200, 200)
-            };
-            Button cadastrarCliente = new Button() {
-                Text = "Cadastrar Cliente",
-                Location = new Point(50, 30),
-                Size = new Size(75, 30)
-            };
-
-            menuPrincipal.Controls.Add(cadastrarCliente);
+        Button btnCadastrarCliente;
+        public static void Main() {
             Application.EnableVisualStyles();
-            Application.Run(menuPrincipal);
+            Application.Run(new Program());
+        }
+        public Program()
+        {
+            btnCadastrarCliente= new Button() {
+                Text = "Cadastrar Cliente",
+                Location = new Point(30, 30),
+                Size = new Size(75, 30),
+            };
+            btnCadastrarCliente.Click += new EventHandler(this.btnCadastrarClienteClick);
+
+            this.Controls.Add(btnCadastrarCliente);
+
+            this.Size = new Size(150, 100);
+            Application.Run(this);
+            
+        }
+
+        private void btnCadastrarClienteClick(object sender, EventArgs e) {
+            Application.Run(new MenuCadastraCLiente());
         }
     }
 }
